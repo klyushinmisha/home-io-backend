3a23cbf982a781fe47114b006b751a8241901e56
mikhail <klyushinmisha@gmail.com>
2019-09-28 04:49:19 +0300

    add multithreading and disable db tracking

--- config/dev_config.py
+++ config/dev_config.py
@@ -1,5 +1,6 @@
 class Config(object):
     DEBUG = True
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2: \
         //postgres:postgres@localhost/home_io_backend"
--- config/test_config.py
+++ config/test_config.py
@@ -1,5 +1,6 @@
 class Config(object):
     TESTING = True
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2: \
         //postgres:postgres@localhost/home_io_backend"
