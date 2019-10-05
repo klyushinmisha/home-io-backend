6aab3bd1e2e0fd25798849d6ebe4dc80d54894d4
mikhail <klyushinmisha@gmail.com>
2019-10-05 12:39:35 +0300

    set postgres container fixed name

--- config/config.py
+++ config/config.py
@@ -1,3 +1,2 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2: \
-        //postgres:postgres@localhost/home_io_backend"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@home_io_backend_postgres/home_io_backend"
--- config/dev_config.py
+++ config/dev_config.py
@@ -2,5 +2,4 @@ class Config(object):
     DEBUG = True
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2: \
-        //postgres:postgres@localhost/home_io_backend"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@home_io_backend_postgres/home_io_backend"
