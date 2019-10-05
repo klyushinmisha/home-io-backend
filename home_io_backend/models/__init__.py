b5b445dd7fc9118b6d83bb6d5642fb5aa2a91157
mikhail <klyushinmisha@gmail.com>
2019-10-05 12:33:38 +0300

    import base models

--- home_io_backend/models/__init__.py
+++ home_io_backend/models/__init__.py
@@ -1,3 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+from .user import User
+from .device import Device
+from .device_log import DeviceLog
+from .device_task import DeviceTask
