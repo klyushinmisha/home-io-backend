001cc6af759237ee4b69ab8002c9d852f58da360
mikhail <klyushinmisha@gmail.com>
2019-10-05 15:36:57 +0300

    add non existing db check

--- home_io_backend/__init__.py
+++ home_io_backend/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_migrate import Migrate
+from sqlalchemy_utils import database_exists, create_database
 
 from .models import *
 from ..config import Config
@@ -14,4 +15,8 @@ def create_app():
     app.db = db
     migrations_dir = os.environ.get('MIGRATIONS_DIR')
     Migrate(app, db, directory=migrations_dir)
+    with app.app_context():
+        app.db.session.enable_baked_queries = True
+    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+        create_database(app.config["SQLALCHEMY_DATABASE_URI"])
     return app
