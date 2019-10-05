5c5cc8edd58381e4f574edb64358579bca83f834
mikhail <klyushinmisha@gmail.com>
2019-10-05 12:25:05 +0300

    add flask migrate ext

--- home_io_backend/create_app.py
+++ home_io_backend/create_app.py
@@ -1,4 +1,8 @@
+import os
+
 from flask import Flask
+from flask_migrate import Migrate
+
 from .models import db
 from ..config import Config
 
@@ -8,4 +12,6 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     app.db = db
+    migrations_dir = os.environ.get('MIGRATIONS_DIR')
+    Migrate(app, db, directory=migrations_dir)
     return app
