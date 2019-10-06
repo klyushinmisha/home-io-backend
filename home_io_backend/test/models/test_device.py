3f3b6895005a88aeb8cdce7603eb45ac3441ee65
mikhail <klyushinmisha@gmail.com>
2019-10-06 17:25:31 +0300

    fix tests
    1) replace device fixture with device_id (prevents session issues)
    2) fix transaction issues (replace commits with flushes)
    3) fix device ids in baked queries

--- home_io_backend/test/models/test_device.py
+++ home_io_backend/test/models/test_device.py
@@ -5,15 +5,15 @@ from sqlalchemy import bindparam
 
 
 @pytest.fixture(scope='function')
-def device(app, db):
+def device_id(app, db):
     with app.app_context():
-
         u = User(
             username='testuser',
             email='testuser@mail.com',
             password='TestPassword'
         )
         db.session.add(u)
+        db.session.flush()
         
         # create device to read
         d = Device(
@@ -21,9 +21,9 @@ def device(app, db):
             device_type=TypeEnum.blinker,
             owner_id=u.id
         )
-        db.session.add(d)
+        u.devices.append(d)
         db.session.commit()
-        return d
+        return d.id
 
 
 class TestDevice:
@@ -40,73 +40,65 @@ class TestDevice:
                 password='TestPassword'
             )
             db.session.add(u)
+            db.session.flush()
 
             d = Device(
                 name=name,
-                device_type=device_type,
-                owner_id=u.id
+                device_type=device_type
             )
-            db.session.add(d)
+            u.devices.append(d)
             db.session.commit()
 
-
-    @pytest.mark.usefixtures('device')
-    def test_read(self, app, db):
+    def test_read(self, app, db, device_id):
         with app.app_context():
             # try to read device
             bq = Device.baked_query + (lambda q: q
-                                     .filter(Device.id == bindparam('device_id'))
-                                     )
+                .filter(Device.id == bindparam('device_id'))
+            )
             bq_params = {
-                'device_id': 1
+                'device_id': device_id
             }
             d = (bq(db.session())
-                 .params(bq_params)
-                 .one_or_none())
+                .params(bq_params)
+                .one_or_none())
             assert d is not None
 
+    @pytest.mark.parametrize(
+        'name, device_type',
+        (('newname', TypeEnum.humidity_sensor),)
+    )
+    def test_update(self, app, db, name, device_type, device_id):
+        with app.app_context():
+            # read user
+            bq = Device.baked_query + (lambda q: q
+                .filter(Device.id == bindparam('device_id'))
+            )
+            bq_params = {
+                'device_id': device_id
+            }
+            d = (bq(app.db.session())
+                .params(bq_params)
+                .one_or_none())
 
-@pytest.mark.parametrize(
-    'name, device_type',
-    (('newname', TypeEnum.humidity_sensor),)
-)
-@pytest.mark.usefixtures('device')
-def test_update(self, app, db, name, device_type,owner_id):
-    with app.app_context():
-        # read user
-        bq = Device.baked_query + (lambda q: q
-                                 .filter(Device.id == bindparam('device_id'))
-                                 )
-        bq_params = {
-            'device_id': 1
-        }
-        d = (bq(app.db.session())
-             .params(bq_params)
-             .one_or_none())
+            # update device data
+            d.name = name
+            d.device_type = device_type
+            db.session.add(d)
+            db.session.commit()
 
-        # update device data
-        d.name = name
-        d.device_type = device_type
-        d.owner_id = owner_id
-        db.session.add(d)
-        db.session.commit()
-
-
-@pytest.mark.usefixtures('device')
-def test_delete(self, app, db):
-    with app.app_context():
-        # read device
-        bq = Device.baked_query + (lambda q: q
-                                 .filter(Device.id == bindparam('device_id'))
-                                 )
-        bq_params = {
-            'device_id': 1
-        }
-        d = (bq(db.session())
-             .params(bq_params)
-             .one_or_none())
-        
-
-        # delete device
-        db.session.delete(d)
-        db.session.commit()
+    def test_delete(self, app, db, device_id):
+        with app.app_context():
+            # read device
+            bq = Device.baked_query + (lambda q: q
+                .filter(Device.id == bindparam('device_id'))
+            )
+            bq_params = {
+                'device_id': device_id
+            }
+            d = (bq(db.session())
+                .params(bq_params)
+                .one_or_none())
+            
+            # delete device
+            db.session.delete(d)
+            db.session.commit()
