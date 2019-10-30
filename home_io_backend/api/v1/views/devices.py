8316c24b3c138379ca54bdf24419e0e98d4dc7a8
utimur <akva_ten@mail.ru>
2019-10-30 18:21:38 +0300

    add device view, create new device method, fix device create schema

---
+++ home_io_backend/api/v1/views/devices.py
@@ -0,0 +1,26 @@
+from webargs.flaskparser import use_kwargs
+
+from .. import api
+from ..responses.device import *
+from ..schemas import DeviceCreateSchema
+from ..view_decorators import json_mimetype_required
+from ....models import Device, db
+
+
+@api.route("/devices", methods=['POST'])
+@json_mimetype_required
+@use_kwargs(DeviceCreateSchema, locations=('json',))
+def create_new_device(name, device_type, owner_id):
+    device = Device.query.filter(
+        Device.name == name
+    ).one_or_none()
+    if device is not None:
+        return DeviceAlreadyExistResponse()
+    device = Device(
+        name=name,
+        device_type=device_type,
+        owner_id=owner_id,
+    )
+    db.session.add(device)
+    db.session.commit()
+    return DeviceResponse(device)
