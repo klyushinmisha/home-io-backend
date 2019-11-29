fe5b834a2f3b15fc36a4361efe3ab71a91d80cf6
mikhail <klyushinmisha@gmail.com>
2019-11-29 15:34:47 +0300

    remove useless code

--- home_io_backend/api/v1/jwt_management.py
+++ home_io_backend/api/v1/jwt_management.py
@@ -5,9 +5,6 @@ from ...models import User
 @jwt.user_loader_callback_loader
 def user_loader_callback(identity):
     with app.app_context():
-        user = User.query.filter(
+        return User.query.filter(
             User.username == identity
         ).one_or_none()
-        if user is None:
-            return None
-        return user
